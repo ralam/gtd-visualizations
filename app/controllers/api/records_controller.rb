e917bf905e5d345dc05c7cc640d8c37a7370114d
ralam <rehan.alam@gmail.com>
2015-11-26 14:12:20 -0800

    add country and year query options to index

--- app/controllers/api/records_controller.rb
+++ app/controllers/api/records_controller.rb
@@ -1,5 +1,13 @@
 class Api::RecordsController < ApplicationController
   def index
-    @records = Record.all
+    if params['year'] && params['country']
+      @records = Record.where({iyear: params['year'], country_txt: params['country']})
+    elsif params['year']
+      @records = Record.where({iyear: params['year']})
+    elsif params['country']
+      @records = Record.where({country_txt: params['country']})
+    else
+      @records.limit(100)
+    end
   end
 end

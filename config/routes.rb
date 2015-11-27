46e8ff83dad3acf3028283f2a905590f5375252d
ralam <rehan.alam@gmail.com>
2015-11-27 12:58:52 -0800

    set root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root to: 'static_pages#index'
+
   resources :static_pages, only: [:index]
   namespace :api, defaults: {format: 'json'}  do
     resources :records, only: [:index]

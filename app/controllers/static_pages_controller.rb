636843057ac536b456082f3b1da9e1849d481357
ralam <rehan.alam@gmail.com>
2015-11-27 13:43:06 -0800

    sort countries and years for dropdown

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,7 @@ class StaticPagesController < ApplicationController
   def index
     @countries = Record.uniq.pluck(:country_txt)
     @years = Record.uniq.pluck(:iyear)
+    @countries.sort!
+    @years.sort!
   end
 end

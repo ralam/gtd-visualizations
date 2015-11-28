178c5cd5babbf9149db0bd1bf5be94caa553ca2b
ralam <rehan.alam@gmail.com>
2015-11-28 15:59:27 -0800

    add piecharts for countries 2 and 3

--- app/assets/javascripts/form_compiler.js
+++ app/assets/javascripts/form_compiler.js
@@ -3,18 +3,20 @@
     window.Display = {};
   };
 
-  var FormParser = Display.FormParser = function (monthsEl, attacksEl, suicidesEl) {
+  var FormParser = Display.FormParser = function (monthsEl, attacksEl, firstPie, secondPie, thirdPie) {
     this.monthsChart = new window.Display.MonthsChart(monthsEl);
     this.attacksChart = new window.Display.AttacksChart(attacksEl);
-    this.suicidesChart = new window.Display.SuicidesChart(suicidesEl);
+    this.suicidesChart1 = new window.Display.SuicidesChart(firstPie);
+    this.suicidesChart2 = new window.Display.SuicidesChart(secondPie);
+    this.suicidesChart3 = new window.Display.SuicidesChart(thirdPie);
   };
 
   FormParser.prototype.handleSubmit = function() {
     $('form').on('submit', function(e){
       e.preventDefault();
       var formData = $(event.currentTarget).serializeJSON();
-      var formID = $(event.currentTarget).attr('id');
-      var Id = formID[formID.length - 1] - 1;
+      var formId = $(event.currentTarget).attr('id');
+      var Id = formId[formId.length - 1] - 1;
       var country = formData.country;
       var year = formData.year;
 
@@ -27,7 +29,19 @@
           console.dir(response);
           this.monthsChart.update(country, year, response.months, Id);
           this.attacksChart.update(country, year, response.attackTypes, Id);
-          this.suicidesChart.update(country, year, response.suicideAttacks, response.nonSuicideAttacks);
+          switch(Id) {
+            case 0:
+              this.suicidesChart1.update(country, year, response.suicideAttacks, response.nonSuicideAttacks);
+              break;
+            case 1:
+              this.suicidesChart2.update(country, year, response.suicideAttacks, response.nonSuicideAttacks);
+              break;
+            case 2:
+              this.suicidesChart3.update(country, year, response.suicideAttacks, response.nonSuicideAttacks);
+              break;
+            default:
+              // no op
+          };
         }.bind(this),
         error: function(XHR, textStatus, errorThrown) {
           console.log(XHR);
